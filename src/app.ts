46e75be582d0e2aee4f25cc0b58ed5ef954040e8
santhosh <masupallisanthosh2006@gmail.com>
2026-07-06 16:57:52 +0530

    upadated security

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express from "express"
 import { errorHandler } from "./middleware/errorHandler"
+import pinoHttp from "pino-http"
+import cors from "cors"
 import routes from "./routes/index"
 const app=express()
 
+app.disable("x-powered-by");
 app.set("trust proxy", true);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json())
+app.use(pinoHttp());
+app.use(routes);
+
 app.use(routes)
 app.use(errorHandler)
 
